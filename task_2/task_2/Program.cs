ae45c6dab4b582206cd7ebe93d5b3161fa114beb
IvanKorolkov <92414605+IvanKorolkov@users.noreply.github.com>
2023-03-19 20:31:39 +0300

    Add files via upload

---
+++ task_2/task_2/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+using System;
+using System.IO;
+
+namespace MyApp 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String line;
+            try
+            {
+                int count = 0;
+                int sum = 0;
+                StreamReader sr = new StreamReader("D:\\projects C#\\task_2\\input.txt");
+                line = sr.ReadLine();
+                while (line != null)
+                {
+                    line = line.Trim();
+                    if (!line.Contains("."))
+                    {
+                        sum += Int32.Parse(line);
+                        count++;   
+                    }
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+                Console.WriteLine("Сумма целых чисел: " + sum);
+                Console.WriteLine("Количество целых чисел: " + count);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+    }
+}
